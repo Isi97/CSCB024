226096d16727d1b593a0eb2946399d5693f35ea0
Isi <isidoros.isp@gmail.com>
2018-12-19 15:19:57 +0200

    Using the help command now clears the screen before displaying available commands

--- commands.py
+++ commands.py
@@ -58,6 +58,7 @@ def getCommand(command):
     elif command == "alarm":
         setAlarm()
     elif command == "help":
+        uinterface.console.Clear()
         printCommandList()
     else:
         pluginManager.executeCommand(command)
